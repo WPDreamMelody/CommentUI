2b9597e36d4a42a29d57cd4dc0099b15e7f52d4a
kang.yu <kang.yu.sh@dianping.com>
2016-08-13 12:02:19 +0800

    add

---
+++ Classes/NVYViewController.h
@@ -0,0 +1,13 @@
+//
+//  NVYViewController.h
+//  Pods
+//
+//  Created by yuki on 16/8/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NVYViewController : NVViewController
+@property(strong)NSString *name;
+@end
---
+++ Classes/NVYViewController.m
@@ -0,0 +1,13 @@
+//
+//  NVYViewController.m
+//  Pods
+//
+//  Created by yuki on 16/8/13.
+//
+//
+
+#import "NVYViewController.h"
+
+@implementation NVYViewController
+
+@end
